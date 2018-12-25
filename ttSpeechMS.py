793824a7dbb35e64d574d8dd2790e1b4213fa40f
dqqcasia <453332177@qq.com>
2018-12-25 14:33:45 +0800

    add microsoft text-to-speech api

---
+++ ttSpeechMS.py
@@ -0,0 +1,47 @@
+import os, requests, time
+
+try: input = raw_input
+except NameError: pass
+
+class TextToSpeech(object):
+    def __init__(self, subscription_key):
+        self.subscription_key = subscription_key
+        self.tts = input("What would you like to convert to speech: ")
+        self.timestr = time.strftime("%Y%m%d-%H%M")
+        self.access_token = None
+
+    def get_token(self):
+        fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.subscription_key
+        }
+        response = requests.post(fetch_token_url, headers=headers)
+        self.access_token = str(response.text)
+
+    def save_audio(self):
+        base_url = 'https://westus.tts.speech.microsoft.com/'
+        path = 'cognitiveservices/v1'
+        constructed_url = base_url + path
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+            'User-Agent': 'YOUR_RESOURCE_NAME',
+            'cache-control': 'no-cache'
+        }
+        body = "<speak version='1.0' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>" + self.tts + "</voice></speak>"
+
+        response = requests.post(constructed_url, headers=headers, data=body)
+        if response.status_code == 200:
+            with open('sample-' + self.timestr + '.wav', 'wb') as audio:
+                audio.write(response.content)
+                print("\nStatus code: " + str(response.status_code) + "\nYour TTS is ready for playback.\n")
+        else:
+            print("\nStatus code: " + str(
+                response.status_code) + "\nSomething went wrong. Check your subscription key and headers.\n")
+
+if __name__ == "__main__":
+    subscription_key = "a4204efb80714d06bf71cbd632719387"
+    app = TextToSpeech(subscription_key)
+    app.get_token()
+    app.save_audio()
